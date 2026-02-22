8dff7c7f565bc53e2429d9ff67786e9edf84b551
samhillsantos <samuel.hillsantos@gmail.com>
2026-02-22 15:02:52 -0300

    reestruturação completa do sistema de cadastro

---
+++ Sistema.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Sistema{
+    public static Scanner input = new Scanner(System.in);
+    // --- Questão 1 (Função Auxiliar de Busca) ---
+
+    // --- Questão 1 (Função Principal) ---
+
+    // --- Questão 2 (Função Auxiliar IMC) ---
+
+    // --- Questão 2 (Função Principal) ---
+
+    // --- Questão 3 ---
+
+    // --- Questão 4 ---
+
+    // --- Questão 5 ---
+    
+    // --- Método Principal (Executor do Sistema) ---
+    
+        // Teste de Cadastro (Q1)
+        
+        // Teste de Impressão (Q2)
+        
+        // Você pode adicionar mais testes aqui para Q3, Q4, Q5
+    public static void main(String[] args){
+
+    }
+}
